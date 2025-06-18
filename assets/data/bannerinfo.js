e0a1be501a85501ff9616c200c06c3457f09112e
mistheo <th.mistral@gmail.com>
2025-06-18 08:42:56 +0200

    Amélioration de la bannière : mise à jour du contenu et du style pour une présentation plus attrayante et informative.

--- assets/data/bannerinfo.js
+++ assets/data/bannerinfo.js
@@ -1,8 +1,8 @@
 window.bannerInoData = {
     bannerInfo: {
-        title: "À la recherche d'une alternance",
-        subtitle: "Je cherche activement une alternance pour la suite de mon master en développement web et gestion de projets informatiques.",
-        description: "Passionné par le développement, je souhaite rejoindre une entreprise pour approfondir mes compétences et contribuer à des projets innovants dès que possible. Je suis peut-être la personne que vous recherchez !",
+        title: "À la recherche d'une alternance créative",
+        subtitle: "Dans le cadre de la suite de mon master en développement et gestion de projets informatiques.",
+        description: "Passionné par le développement et la création, je souhaite rejoindre une entreprise innovante pour approfondir mes compétences et contribuer à des projets originaux dès que possible. Si vous recherchez un profil créatif et motivé, je suis surement la personne qu'il vous faut.",
         ctaText: "Mon parcours",
         ctaLink: "#experiences",
     },
