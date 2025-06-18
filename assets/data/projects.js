f9308d654911819179ebc76722eb362cc3a78316
mistheo <th.mistral@gmail.com>
2025-06-18 13:28:11 +0200

    Correction de la description du projet LeVivier pour une meilleure clarté.

--- assets/data/projects.js
+++ assets/data/projects.js
@@ -3,7 +3,7 @@ window.projectsData = [
         "id": 1,
         "title": "Site Web Vitrine LeVivier",
         "date": "2025",
-        "description": "Développement d'un site vitrine pour une entreprise de restauration Lyonnaise avec interface utilisateur moderne et système de formulaire de contact.",
+        "description": "Développement d'un site vitrine pour une entreprise de restauration Lyonnaise avec interface utilisateur moderne et formulaire de contact.",
         "technologies": ["Python", "Flask", "HTML/JS/CSS","SMTP"],
         "image": "../assets/img/projects/levivier_index.png",
         // "github": "https://github.com/mistheo/levivier",
