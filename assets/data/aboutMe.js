b87bd05d9db4653eea3acbd2f4aeb76f7250a0f2
mistheo <th.mistral@gmail.com>
2025-06-18 12:59:56 +0200

    Ajout du fichier aboutMe.js pour centraliser les informations personnelles et mise à jour de index.html pour intégrer ces données dynamiquement.

---
+++ assets/data/aboutMe.js
@@ -0,0 +1,15 @@
+window.aboutMe = {
+    "description": `Je m'appelle Théo,<br><br>
+                    Etudiant, en alternance dans le domaine de l'informatique,<br>
+                    Basé à Lyon.<br><br>
+                    Passionné par le développement logiciel et la gestion de projets (et la musique !), je souhaite m'épanouir dans la création de projets innovants et créatifs via ma passion de l'informatique.
+                    Ma méthodologie allie, rigueur technique et créativité pour transformer les idées en projets concrets.`,
+    "valeurs" : [
+        "Créativité",
+        "Rigueur",
+        "Passion",
+        "Esprit d'équipe",
+        "Curiosité",
+        "Persévérance"
+    ]
+}
