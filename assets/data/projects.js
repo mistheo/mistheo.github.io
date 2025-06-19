7f7d7fe50fc0a33fe8b2c5e8be9bcd672dd489c2
mistheo <th.mistral@gmail.com>
2025-06-19 09:22:31 +0200

    Ajout de nouveaux projets au fichier projects.js et intégration des images correspondantes. Mise à jour de index.html pour améliorer l'affichage des liens de démo avec des attributs de sécurité.

--- assets/data/projects.js
+++ assets/data/projects.js
@@ -7,5 +7,26 @@ window.projectsData = [
         "technologies": ["Python", "Flask", "HTML/JS/CSS","SMTP"],
         "image": "../assets/img/projects/levivier_index.png",
         // "github": "https://github.com/mistheo/levivier",
+    },
+    {
+        "id":2,
+        "title": "Jeu des couronnes",
+        "date": "2025",
+        "description": "Redeveloppement du jeu des couronnes, repris du jeu sur LinkedIn. Ce jeu de logique, génére une grille de jeu aléatoire où il faut placer un couronne par région, par colonnes et par lignes.",
+        "technologies": ["JavaScript", "HTML/CSS", "AlpineJS","tailwindCSS"],
+        "image": "../assets/img/projects/crowns.png",
+        "github": "https://github.com/mistheo/crown",
+        "demo":"https://mistheo.github.io/crown/",
+    },
+    {
+        "id": 3,
+        "title": "Script de notification Twitch via Webhook Discord",
+        "date": "2024",
+        "description": "Script Python permettant de notifier un serveur Discord via Webhook lorsqu'un streamer Twitch live sur sa chaine.",
+        "technologies": ["Python", "Discord API", "Twitch API"],
+        "image": "../assets/img/projects/discord_twitch.png",
+        "github": "https://github.com/mistheo/twitch_alterte_webhook",
+        "demo": "https://mistheo.github.io/twitch_alterte_webhook/",
     }
+
 ];
